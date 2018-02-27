dd120b72a1820f518e08ace6e159cddbcb74b5da
Xun <zhuxun2@gmail.com>
2018-02-26 18:46:05 -1000

    commit

---
+++ public/p1-process.py
@@ -0,0 +1,102 @@
+import csv
+import requests
+import os
+import errno
+from time import sleep
+from bs4 import Tag, BeautifulSoup, NavigableString
+from colors import color
+import re
+from sys import exit
+
+import signal
+
+class TimeoutError(Exception):
+    pass
+
+class Timeout:
+    def __init__(self, seconds=1, error_message='Timeout'):
+        self.seconds = seconds
+        self.error_message = error_message
+    def handle_timeout(self, signum, frame):
+        raise TimeoutError(self.error_message)
+    def __enter__(self):
+        signal.signal(signal.SIGALRM, self.handle_timeout)
+        signal.alarm(self.seconds)
+    def __exit__(self, type, value, traceback):
+        signal.alarm(0)
+
+keywords = [
+    'gender equality',
+    'female',
+    'male',
+    'board diversity',
+    'diverse',
+]
+
+
+def is_toc_link(el):
+    return isinstance(el, Tag) and el.name == 'a' and el.has_attr(
+        'href') and 'table of content' in el.get_text().lower()
+
+
+def if_page_break_el(el):
+    return el.has_attr('style') and (
+        'page-break-before' in el['style'].lower()
+        or 'page-break-after' in el['style'].lower())
+
+
+def is_toc(toc_name):
+    return lambda el: el.has_attr('name') and el['name'] == toc_name
+
+
+def to_text(el):
+    return el.get_text() if isinstance(el, Tag) else str(el)
+
+
+def process_row(i, row):
+    global num_links
+
+    # if int(row['num_pages_with_proposal']) <= 4:
+    #     return
+
+    try:
+        with Timeout(seconds=120):
+            link_to_doc = 'https://www.sec.gov/Archives/' + row['Filename']
+            index = row['CIK']
+            filename = './o1-htm_files/' + row['Filename'] + '.htm'
+
+            res_text = requests.get(link_to_doc).text
+
+            print('{:7} : ({}/{}) {} ({} bytes)'.format(index, i, num_links, filename, len(res_text)))
+
+            if not os.path.exists(os.path.dirname(filename)):
+                try:
+                    os.makedirs(os.path.dirname(filename))
+                except OSError as exc:
+                    if exc.errno != errno.EEXIST:
+                        raise
+
+            with open(filename, 'w') as ofile:
+                ofile.write(res_text)
+
+    except:
+        print('HEY!!!!!!!!!')
+        return None
+
+
+def main():
+    global num_links
+    with open('./list.csv') as input_file:
+        input_table = list(csv.DictReader(input_file))
+        input_table = [x for x in input_table if int(x['num_pages_with_shareholder_proposal']) > 5]
+
+    num_links = len(input_table)
+
+    for i, row in enumerate(input_table):
+        process_row(i, row)
+        sleep(0.15)
+
+    # Parallel(n_jobs=50)([delayed(process_row)(i, row) for i, row in enumerate(input_table)])
+
+if __name__ == '__main__':
+    main()
---
+++ src/App.js
@@ -0,0 +1,119 @@
+import React from 'react';
+import './App.css';
+import { Button, Paper, Typography as T } from 'material-ui';
+import { withState, compose, lifecycle, withProps, branch, renderNothing } from 'recompose';
+import { HotKeys } from 'react-hotkeys';
+import update from 'immutability-helper';
+import Papa from 'papaparse';
+
+const download = (filename, text) => {
+  const element = document.createElement('a');
+  element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+  element.setAttribute('download', filename);
+
+  element.style.display = 'none';
+  document.body.appendChild(element);
+
+  element.click();
+
+  document.body.removeChild(element);
+};
+
+const App = ({ save, yes, no, list, idx, prevLink, nextLink }) => (
+  <HotKeys
+    keyMap={{
+      prevLink: ['a', 'left'],
+      nextLink: ['d', 'right'],
+      no: 'w',
+      yes: 'e',
+      save: 's',
+    }}
+    handlers={{
+      prevLink,
+      nextLink,
+      yes,
+      no,
+      save,
+    }}
+  >
+    <div className="app">
+      <div className="buttonTray">
+        <div className="buttonRow">
+          <Paper
+            className={[
+              'indexDisplay',
+              list[idx].human_judgement === 'no_proposal' ? 'red' : '',
+              list[idx].human_judgement === 'found_proposal' ? 'green' : '',
+            ].join(' ')}
+          >
+            <T classes={{ root: 'indexDisplayText' }}>
+              <strong>
+                Document {idx + 1} / {list.length}
+              </strong>{' '}
+              : {list[idx]['Company Name']} {list[idx]['Date Filed']}{' '}
+              <a href={`/o1-htm_files/${list[idx].Filename}.htm`}>{`/o1-htm_files/${list[idx].Filename}.htm`}</a>
+              {list[idx].human_judgement && ` (${list[idx].human_judgement})`}
+            </T>
+          </Paper>
+        </div>
+        <div className="buttonRow right">
+          <Button classes={{ root: 'button' }} variant="raised" size="small" color="primary" onClick={prevLink}>
+            Previous (A or &larr;)
+          </Button>
+          <Button classes={{ root: 'button' }} variant="raised" size="small" color="primary" onClick={nextLink}>
+            Next (D or &rarr;)
+          </Button>
+          <Button classes={{ root: 'button red' }} variant="raised" size="small" onClick={no}>
+            No proposal (W)
+          </Button>
+          <Button classes={{ root: 'button green' }} variant="raised" size="small" onClick={yes}>
+            Found proposal (E)
+          </Button>
+          <Button classes={{ root: 'button' }} variant="raised" size="small" onClick={save}>
+            Save (S)
+          </Button>
+        </div>
+      </div>
+      <iframe src={`/o1-htm_files/${list[idx].Filename}.htm`} title="htm" className="viewer" width="100%" height="300" />
+    </div>
+  </HotKeys>
+);
+
+const enhance = compose(
+  withState('list', 'setList', []),
+  withState('idx', 'setIdx', 0),
+  withProps(({ list, setList, idx, setIdx }) => ({
+    prevLink: () => {
+      if (idx > 0) {
+        setIdx(idx - 1);
+      }
+    },
+    nextLink: () => {
+      if (idx < list.length - 1) {
+        setIdx(idx + 1);
+      }
+    },
+    yes: () => {
+      setList(update(list, { [idx]: { human_judgement: { $set: 'found_proposal' } } }));
+    },
+    no: () => {
+      setList(update(list, { [idx]: { human_judgement: { $set: 'no_proposal' } } }));
+    },
+    save: () => {
+      download('list.csv', Papa.unparse(list));
+    },
+  })),
+  lifecycle({
+    componentWillMount() {
+      setImmediate(async () => {
+        const res = await fetch('/list.csv');
+        const listCsv = await res.text();
+        const list = Papa.parse(listCsv, { header: true });
+        this.props.setList(list.data.filter(x => x.num_pages_with_shareholder_proposal > 5));
+      });
+    },
+  }),
+  branch(({ list }) => list.length === 0, renderNothing),
+);
+
+export default enhance(App);
